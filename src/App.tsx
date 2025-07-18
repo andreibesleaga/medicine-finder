b22be5b1bcd0675292888e20f48e5d132aa5acc4
Andrei Besleaga <andrei@LAPTOP-9JJ14NEU.localdomain>
2025-07-18 12:04:54 +0300

    medicine-finder - change: added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/react"
 import { DatabaseManager } from "./components/DatabaseManager";
 
 const queryClient = new QueryClient({
@@ -30,6 +31,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
